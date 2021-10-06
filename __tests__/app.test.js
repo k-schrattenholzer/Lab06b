736ab20cced71f58a99732b4d6fe485996f07f27
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-05 20:00:44 -0700

    update to pass checks

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -54,217 +54,217 @@ describe('app routes', () => {
     test('returns all quotes', async() => {
       const expectation = [
         {
-          id: 1,
+          id: expect.any(Number),
           character_name: 'Garnet',
           character_id: 1,
           quote:'Yes... Or, you can link your mind with the energy of all existing matter, channeling the collective power of the universe through your gem! Which results in.. *summons her weapon* At least that\'s my way of doing it.',
           img: expect.any(String)
         },
         {
-          id: 2,
+          id: expect.any(Number),
           character_name: 'Garnet',
           character_id: 1,
           quote:'Love at first sight doesn\'t exist. Love takes time and love takes work.',
           img: expect.any(String)
         },
         {
-          id: 3,
+          id: expect.any(Number),
           character_name: 'Garnet',
           character_id: 1,
           quote:'You are not two people, and you are not one person. You… are an experience, and make sure you’re a good experience! Now, Go. Have. FUN.',
           img: expect.any(String)
         },
         {
-          id: 4,
+          id: expect.any(Number),
           character_name: 'Garnet',
           character_id: 1,
           quote:'Steven: Woah. We made it? Garnet: I carried you while you took a nap.',
           img: expect.any(String)
         },
         {
-          id: 5,
+          id: expect.any(Number),
           character_name: 'Stevonnie',
           character_id: 2,
           quote:'I wish you were here. If we were together, it would be okay. But we are together, and it\'s not. I\'m alone.',
           img: expect.any(String)
         },
         {
-          id: 6,
+          id: expect.any(Number),
           character_name: 'Stevonnie',
           character_id: 2,
           quote:'You wanna dance? Let\'s go. And it\'s Stevonnie; I am not your baby.',
           img: expect.any(String)
         },
         {
-          id: 7,
+          id: expect.any(Number),
           character_name: 'Stevonnie',
           character_id: 2,
           quote:'You have so many worlds and I don\'t even have one! It\'s not fair! I. Want. ONE! I want my own army! I want my own planet! I deserve it! I\'m just as important as you!',
           img: expect.any(String)
         },
         {
-          id: 8,
+          id: expect.any(Number),
           character_name: 'Smoky Quartz',
           character_id: 3,
           quote:'Don\'t bother putting your socks back on, \'cause I\'m about to knock \'em off again!',
           img: expect.any(String)
         },
         {
-          id: 9,
+          id: expect.any(Number),
           character_name: 'Smoky Quartz',
           character_id: 3,
           quote:'Holy smokes! I\'m Smoky! Steven, you beautiful genius! You pulled Amethyst out of her gem through fusion?!',
           img: expect.any(String)
         },
         {
-          id: 10,
+          id:expect.any(Number),
           character_name: 'Smoky Quartz',
           character_id: 3,
           quote:'Well, sometimes you save all the people, but the roller coaster still crashes into the ocean.  (The roller coaster explodes in the water.) And that\'s okay.',
           img: expect.any(String)
         },
         {
-          id: 11,
+          id: expect.any(Number),
           character_name: 'Rose Quartz',
           character_id: 4,
           quote:'Isn\'t it remarkable, Steven? This world is full of so many possibilities. Each living thing has an entirely unique experience. The sights they see, the sounds they hear. The lives they live are so complicated... and so simple. I can\'t wait for you to join them.',
           img: expect.any(String)
         },
         {
-          id: 12,
+          id: expect.any(Number),
           character_name: 'Rose Quartz',
           character_id: 4,
           quote:'Who cares about how I feel? How you feel is bound to be much more interesting.',
           img: expect.any(String)
         },
         {
-          id: 13,
+          id: expect.any(Number),
           character_name: 'Rose Quartz',
           character_id: 4,
           quote:'No more questions. Don\'t ever question this. You already are the answer.',
           img: expect.any(String)
         },
         {
-          id: 14,
+          id: expect.any(Number),
           character_name: 'Rose Quartz',
           character_id: 4,
           quote:'When a Gem is made, it\'s for a reason. They burst out of the ground already knowing what they\'re supposed to be, and then... that\'s what they are. Forever. But you, you\'re supposed to change. You\'re never the same even moment to moment -- you\'re allowed and expected to invent who you are. What an incredible power -- the ability to "grow up."',
           img: expect.any(String)
         },
         {
-          id: 15,
+          id: expect.any(Number),
           character_name: 'Pearl',
           character_id: 5,
           quote:'Steven, until you learn to control the powers in your gem, *snaps Centipeetle’s neck* we’ll take care of protecting humanity, okay?',
           img: expect.any(String)
         },
         {
-          id: 16,
+          id: expect.any(Number),
           character_name: 'Pearl',
           character_id: 5,
           quote:'They don\'t just look like her, they are like her. She made them. I thought I\'d be more ready for this...',
           img: expect.any(String)
         },
         {
-          id: 17,
+          id: expect.any(Number),
           character_name: 'Amethyst',
           character_id: 6,
           quote:'In the ring, nobody can tell me what to do! And if they try, I HIT \'EM IN THE FACE WITH A CHAIR!',
           img: expect.any(String)
         },
         {
-          id: 18,
+          id: expect.any(Number),
           character_name: 'Amethyst',
           character_id: 6,
           quote:'I only feel how I wanna feel.',
           img: expect.any(String)
         },
         {
-          id: 19,
+          id: expect.any(Number),
           character_name: 'Ruby',
           character_id: 7,
           quote:'You\'re not, as above this as you think you are! *groans angrily*',
           img: expect.any(String)
         },
         {
-          id: 20,
+          id: expect.any(Number),
           character_name: 'Ruby',
           character_id: 7,
           quote:'Why does she always act like I\'m being ridiculous? Just because she *mumbles underwater*',
           img: expect.any(String)
         },
         {
-          id: 21,
+          id: expect.any(Number),
           character_name: 'Sapphire',
           character_id: 8,
           quote:'Ruby: How am I gonna save you? Sapphire: You already did. Ruby: What!? Sapphire: You already saved me.',
           img: expect.any(String)
         },
         {
-          id: 22,
+          id: expect.any(Number),
           character_name: 'Sapphire',
           character_id: 8,
           quote:'We won\'t stand out if we play the roles we were made for.',
           img: expect.any(String)
         },
         {
-          id: 23,
+          id: expect.any(Number),
           character_name: 'Peridot',
           character_id: 9,
           quote:'You Gems really are as dull as dirt.',
           img: expect.any(String)
         },
         {
-          id: 24,
+          id: expect.any(Number),
           character_name: 'Peridot',
           character_id: 9,
           quote:'Steven! You never gave up on me for reasons I don\'t understand! I\'ll do the same for you!',
           img: expect.any(String)
         },
         {
-          id: 25,
+          id: expect.any(Number),
           character_name: 'Lapis Lazuli',
           character_id: 10,
           quote:'Can\'t you see? I can\'t stop, not for a second. Don\'t look for me. I don\'t want your help!',
           img: expect.any(String)
         },
         {
-          id: 26,
+          id: expect.any(Number),
           character_name: 'Lapis Lazuli',
           character_id: 10,
           quote:'For a moment, I really felt like things were different, but they\'re not. No matter where I go, I\'m trapped.',
           img: expect.any(String)
         },
         {
-          id: 27,
+          id: expect.any(Number),
           character_name: 'Steven Universe',
           character_id: 11,
           quote:'Sleep is a curse, and yet a curse I need to live.',
           img: expect.any(String)
         },
         {
-          id: 28,
+          id: expect.any(Number),
           character_name: 'Greg Universe',
           character_id: 12,
           quote:'If every pork-chop were perfect, we wouldn\'t have hotdogs.',
           img: expect.any(String)
         },
         {
-          id: 29,
+          id: expect.any(Number),
           character_name: 'Lars Barriga',
           character_id: 13,
           quote:'Do you ever get lonely? Even when you\'re around people?',
           img: expect.any(String)
         },
         {
-          id: 30,
+          id: expect.any(Number),
           character_name: 'Sadie Miller',
           character_id: 14,
           quote:'Yeah, you know what? Yeah! Who cares, right? What am I afraid of? This could be fun!',
           img: expect.any(String)
         },
         {
-          id: 31,
+          id: expect.any(Number),
           character_name: 'Connie Maheswaran',
           character_id: 15,
           quote:'Of course it is. I like you just the way you are.',
